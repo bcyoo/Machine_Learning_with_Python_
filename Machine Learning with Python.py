cef229e06ff20da4fa6ea80d3e0ee13d74136698
bcyoo <89197951+bcyoo@users.noreply.github.com>
2021-11-17 23:22:52 +0900

    Add files via upload

---
+++ Machine Learning with Python.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+$pip install jupytext $jupyter notebook --generate-config
+
+출처: https://sexydatadesigner.tistory.com/entry/jupyter-notebook-과-git-연동하기 [Frank and plain, Vingle]
+# -
+
+import numpy as np
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print ('x:\n',x)
